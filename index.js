b64a52ab9c30f4f5967a7145e02b07bfebbd9c19
John Saviour <johnsaviour88@gmail.com>
2023-01-18 11:04:47 +0100

    added registration and login feature

--- index.js
+++ index.js
@@ -5,33 +5,10 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
 // Internal Imports
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
-const Post = require("./models/post.js");
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts:[Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
